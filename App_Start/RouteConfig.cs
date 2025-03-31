c401d1691e21c9596180c4369950f180454b6bad
AdrianMM12345 <adrian.marcelomonje2002@gmail.com>
2025-03-30 23:12:39 -0500

    Initial commit (reinicio completo)1

---
+++ App_Start/RouteConfig.cs
@@ -0,0 +1,19 @@
+﻿using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace webInstagram_MVC
+{
+    public class RouteConfig
+    {
+        public static void RegisterRoutes(RouteCollection routes)
+        {
+            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+
+            routes.MapRoute(
+                name: "Instagram",
+                url: "{controller}/{action}/{id}",
+                defaults: new { controller = "Instagram", action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
---
+++ Controllers/InstagramController.cs
@@ -0,0 +1,65 @@
+﻿using System.Web.Mvc;
+
+namespace webInstagram_MVC.Controllers
+{
+    public class InstagramController : Controller
+    {
+        [HttpGet]
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Login(FormCollection formCollection)
+        {
+            return RedirectToAction("Muro");
+        }
+
+        [HttpGet]
+        public ActionResult Olvidar()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Olvidar(FormCollection formCollection)
+        {
+            return RedirectToAction("OlvidarCodigo");
+        }
+
+        public ActionResult OlvidarCodigo()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult OlvidarCodigo(FormCollection formCollection)
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult Registrar()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Registrar(FormCollection formCollection)
+        {
+            return View();
+        }
+
+        public ActionResult Muro()
+        {
+            return View();
+        }
+    }
+}
